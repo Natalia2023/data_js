8800026d833b2ee01fd2e4f0853b6d143aff4cd5
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-04-09 19:24:11 +0300

    личильник

--- index.js
+++ index.js
@@ -5,4 +5,27 @@
 // 4)конвертувати різницю в нормальний формат для користувача
 // 5)відобразити це все на екран
 
-const deadLine = new Date(2024, 0, 1);
+const deadLine = new Date(2023, 4, 26);
+const daysElement = document.querySelector(".days");
+const hoursElement = document.querySelector(".hours");
+const minuresElement = document.querySelector(".minutes");
+const secondsElement = document.querySelector(".seconds");
+function timer () {
+
+       const today = new Date();
+    const delta = deadLine - today;
+     
+    const seconds = Math.floor(delta / 1000) % 60 ;
+      const minutes = Math.floor(delta / 1000 / 60) % 60 ;
+     const hours = Math.floor(delta / 1000 / 60 / 60) % 24 ;
+     const days = Math.floor(delta / 1000 / 60 / 60 / 24);
+     
+daysElement.textContent = days;
+hoursElement.textContent = hours;
+minuresElement.textContent = minutes;
+secondsElement.textContent = seconds;
+
+     
+    
+}
+setInterval(timer, 1000);
